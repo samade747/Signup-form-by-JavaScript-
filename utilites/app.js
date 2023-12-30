72cfed5551c1a7ce2e14c0a1bcab3ee0974e4f4d
samade747 <samad.e747@gmail.com>
2023-12-30 09:16:12 +0500

    914AM 30 12 2023 FIREBASE IMPLEMENTATION .push

---
+++ utilites/app.js
@@ -0,0 +1,22 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+  import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBN_7ETQNDk_tfdoEOWbuUgUNQcylUf1Ew",
+    authDomain: "social-media-app-67c6c.firebaseapp.com",
+    projectId: "social-media-app-67c6c",
+    storageBucket: "social-media-app-67c6c.appspot.com",
+    messagingSenderId: "540460296890",
+    appId: "1:540460296890:web:57b9ead047e37123d7b083"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth();
+
+  export { auth }
+
+
