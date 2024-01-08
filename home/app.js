711b2bc55f5b9ec0b759449c3503b172eaea0f8f
samade747 <samad.e747@gmail.com>
2024-01-08 20:06:51 +0500

    806PM 8 1 2024

--- home/app.js
+++ home/app.js
@@ -1,4 +1,4 @@
-import { getData, getLoggedInUser } from "../utilites/functions.mjs";
+import { addInDB, getAllDataOrderedByTimestamp, getData, getLoggedInUser, uploadFile } from "../utilites/functions.mjs";
 
 const postInput = document.querySelector("#postInput");
 const postContentArea = document.querySelector("#postContentArea");
@@ -8,181 +8,153 @@ const logoutBtn = document.querySelector("#logoutBtn");
 const ppimage = document.querySelectorAll('.ppimage')
 console.log(ppimage)
 
-// checking login functionality 
-const checkLogin = async () => {
-  console.log('===> checkig login user')
-  const loggedInUser = await getLoggedInUser()
-  if(loggedInUser){
-    console.log('===> user logged in ', loggedInUser)
+let uid;
 
-  // getting user data from database
-  const userData = await getData(loggedInUser.uid, 'users')
-    if(userData.status){
-      ppimage.src = userData.data.ppimage || 'https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper.png'
+const checkLogin = async () => {
+  console.log("===>>> checking login user")
+  const loggedInUser = await getLoggedInUser()
+  uid = loggedInUser.uid
+  if (loggedInUser) {
+    console.log("===>>> user logged in", loggedInUser)
+
+    // get user data from db
+    const userData = await getData(loggedInUser.uid, "users")
+    if (userData.status) {
+      console.log("===>>> user data", userData.data)
+      profilePicture.src = userData.data.profilePicture || "https://t3.ftcdn.net/jpg/05/00/54/28/360_F_500542898_LpYSy4RGAi95aDim3TLtSgCNUxNlOlcM.jpg"
     } else {
       console.log("===>>> user data not found")
     }
   } else {
-    console.log('===> user not logged in ')
-    window.location.href = '../index.html'
+    console.log("===>>> user not logged in")
+    window.location.href = "../index.html"
   }
-
 }
 
 checkLogin()
 
+const postDisplayHandler = async () => {
+  console.log("===>>> post display handler")
+  const posts = await getAllDataOrderedByTimestamp("posts")
+  console.log("===>>> posts", posts)
+  if (posts.status) {
+
+    // Use Promise.all to await all promises in the loop
+    const postsWithDataPromises = posts.data.map(async (post) => {
+      const userData = await getData(post.authorId, "users");
+
+      console.log(userData, "===>>userData")
+
+      // Add user data to the post
+      const postWithUserData = {
+        ...post,
+        userData: userData ? userData.data : null,
+      };
+
+      return postWithUserData;
+    });
+
+    // Wait for all promises to resolve
+    const postsWithData = await Promise.all(postsWithDataPromises);
+
+    console.log("===>>> posts with data", postsWithData)
+
+    renderPosts(postsWithData)
+  } else {
+    console.log("===>>> posts not found")
+  }
+}
+
+postDisplayHandler()
+
+const renderPosts = (posts) => {
+  console.log("===>>> render posts", posts)
+  postContentArea.innerHTML = ""
+  posts.forEach(post => {
+    postContentArea.innerHTML += `
+    <div class="card text-center">
+    <div class="card-header" id="userName">
+        ${post.userData.userName || "No User Name"}
+    </div>
+    <div class="card-body">
+        <h5 class="card-title">Heading</h5>
+        <p class="card-text">${post.post}</p>
+        ${post.imageUrl && `<img src="${post.imageUrl}" class="card-img-top" alt="...">`}
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+    <div class="card-footer text-body-secondary">
+        ${post.userData.email}
+    </div>
+</div>
+    `
+  });
+}
+
+let imageUrl;
+
+const postSubmitHandler = async () => {
+  postContentArea.innerHTML = "Loading"
+  console.log("===>>> post submit handler")
+  if (!postInput.value) {
+    alert("Please enter post")
+    return
+  }
+
+  const data = {
+    post: postInput.value,
+    authorId: uid,
+  }
+
+  // upload image to storage
+  if (imageInput.files[0]) {
+    const imageName = `${new Date().getTime()}-${imageInput.files[0].name}`
+    const upload = await uploadFile(imageInput.files[0], imageName)
+    if (upload.status) {
+      data.imageUrl = upload.downloadURL
+      alert(upload.message)
+    } else {
+      alert(upload.message)
+    }
+  }
 
 
-// import {
-//   addDoc,
-//   auth,
-//   collection,
-//   db,
-//   getDocs,
-//   onAuthStateChanged,
-//   query,
-//   signOut,
-//   doc as docFromFirebase,
-//   doc,
-//   deleteDoc,
-//   setDoc,
-// } from "../utilites/app.js";
+  const postAddInDB = await addInDB(data, "posts")
+  if (postAddInDB.status) {
+    alert(postAddInDB.message)
+    postInput.value = ""
+    imageInput.value = ""
+    postDisplayHandler()
+  } else {
+    alert(postAddInDB.message)
+  }
 
-// let userDetails;
-// let uid;
+}
 
-// // Checking the authentication state of the user
-// onAuthStateChanged(auth, async (user) => {
-//   if (user) {
-//       // If user is signed in
-//       uid = user.uid;
-//       const docRef = doc(db, "users", uid);
-//       const docSnap = await getDoc(docRef);
+submitBtn.addEventListener('click', postSubmitHandler)
 
-//       if (docSnap.exists()) {
-//           userDetails = docSnap.data();
-//       } else {
-//           console.log("No Document found");
-//       }
+
+// // checking login functionality 
+// const checkLogin = async () => {
+//   console.log('===> checkig login user')
+//   const loggedInUser = await getLoggedInUser()
+//   if(loggedInUser){
+//     console.log('===> user logged in ', loggedInUser)
+
+//   // getting user data from database
+//   const userData = await getData(loggedInUser.uid, 'users')
+//     if(userData.status){
+//       ppimage.src = userData.data.ppimage || 'https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper.png'
+//     } else {
+//       console.log("===>>> user data not found")
+//     }
 //   } else {
-//       // If user is signed out, redirect to login page
-//       window.location.href = "../login/index.html";
-//   }
-// });
-
-// // DOM elements
-// const postInput = document.querySelector("#postInput");
-// const postContentArea = document.querySelector("#postContentArea");
-// const submitBtn = document.querySelector("#submitBtn");
-// const imageBtn = document.querySelector("#imageBtn");
-// const logoutBtn = document.querySelector("#logoutBtn");
-
-// let imageUrl;
-
-// // Function to display posts
-// const postDisplayHandler = async () => {
-//   postContentArea.innerHTML = "";
-
-//   // Fetching posts from Firebase
-//   const posts = [];
-//   const q = query(collection(db, "posts"));
-//   const querySnapshot = await getDocs(q);
-
-//   querySnapshot.forEach(async (doc) => {
-//       const singlePost = doc.data();
-//       posts.push({ ...singlePost, id: doc.id });
-//   });
-
-//   // Displaying posts in reverse order
-//   posts.reverse().forEach((post) => {
-//       let textHTML;
-//       // Your card template remains unchanged
-//       postContentArea.innerHTML += textHTML;
-//   });
-// };
-
-// postDisplayHandler();
-
-// // Function to handle image input
-// const imageOpenerHandler = () => {
-//   imageUrl = prompt("Enter the link of your image");
-// };
-
-// // Function to handle post submission
-// window.postSubmitHandler = async () => {
-//   let postObj;
-
-//   // Checking if an image URL is provided
-//   if (imageUrl) {
-//       postObj = {
-//           textData: postInput.value,
-//           imgData: imageUrl,
-//           userDetails: userDetails,
-//       };
-//   } else {
-//       postObj = {
-//           textData: postInput.value,
-//           userDetails: userDetails,
-//       };
-//   }
-
-//   alert("Saving data...");
-
-//   // Adding post to Firebase
-//   const docRef = await addDoc(collection(db, "posts"), postObj);
-//   imageUrl = "";
-//   postInput.value = "";
-//   postDisplayHandler();
-// };
-
-// // Function to handle user logout
-// const logoutHandler = () => {
-//   // Signing out user and redirecting to login page
-//   signOut(auth)
-//       .then(() => {
-//           window.location.href = "../login/index.html";
-//       })
-//       .catch((error) => {
-//           console.log(error);
-//       });
-// };
-
-// // Function to handle post editing
-// window.editHandler = (postId) => {
-//   submitBtn.innerHTML = "Update";
-//   submitBtn.setAttribute("onclick", `updatePostHandler('${postId}')`);
-// };
-
-// // Function to handle post deletion
-// window.deleteHandler = async (postId) => {
-//   await deleteDoc(doc(db, "posts", postId));
-//   postDisplayHandler();
-// };
-
-// // Function to handle updating a post
-// window.updatePostHandler = async (postId) => {
-//   let postObj;
-
-//   // Checking if an image URL is provided
-//   if (imageUrl) {
-//       postObj = {
-//           textData: postInput.value,
-//           imgData: imageUrl,
-//           userDetails: userDetails,
-//       };
-//   } else {
-//       postObj = {
-//           textData: postInput.value,
-//           userDetails: userDetails,
-//       };
+//     console.log('===> user not logged in ')
+//     window.location.href = '../index.html'
 //   }
 
-//   // Updating post in Firebase
-//   await setDoc(doc(db, "posts", postId), postObj);
-//   postDisplayHandler();
-//   submitBtn.innerHTML = "Submit";
-//   submitBtn.setAttribute("onclick", "postSubmitHandler()");
-// };
+// }
+
+// checkLogin()
 
 // // Event listeners
 // logoutBtn.addEventListener("click", logoutHandler);
