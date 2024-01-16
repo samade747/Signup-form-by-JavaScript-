8e24039c6db8ed98a09dd152b439453c5a9e1f65
samade747 <samad.e747@gmail.com>
2024-01-16 13:17:31 +0500

    117PM 16 1 2024

--- utilites/app.js
+++ utilites/app.js
@@ -9,7 +9,6 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import {
   getFirestore,
-  addInDB,
   collection,
   addDoc,
   setDoc,
@@ -23,7 +22,6 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 import {
   ref,
-  addInDB,
   getStorage,
   getDownloadURL,
   uploadBytes
@@ -32,7 +30,8 @@ import {
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-  // Your web app's Firebase configuration
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
   const firebaseConfig = {
     apiKey: "AIzaSyBN_7ETQNDk_tfdoEOWbuUgUNQcylUf1Ew",
     authDomain: "social-media-app-67c6c.firebaseapp.com",
@@ -70,7 +69,5 @@ export {
   getDownloadURL,
   uploadBytes,
   serverTimestamp,
-  orderBy,
-  addInDB  
-  
+  orderBy
 };
